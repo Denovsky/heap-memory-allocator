80048843992438476daad7474e6f14141fb02089
Denovsky <ilya20002282000@mail.ru>
2025-07-26 00:48:57 +0300

    Main logic completed. Working version

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 Just a myself-writen test memory allocator for education purpose.
 
+Status: 85% completed. Main logic is done (malloc, realloc, free).
+
 Plans:
 
-0. Malloc/realloc simulation;
-1. Memory cleaner (free func);
+0. ~~Malloc/realloc simulation;~~
+1. ~~Memory cleaner (free func);~~
 2. Separate memory allocation logic from main file;
-3. Add header block for meta-info;
-4. Fix malloc Segmentaion fault after else condition;
-5. ...
+3. ~~Add header block for meta-info;~~
+4. ~~Fix malloc Segmentaion fault after else condition;~~
+5. Memory defragmentation;
+6. ...
--- allocator.c
+++ allocator.c
@@ -11,11 +11,11 @@ typedef struct header
     struct header *next;
 } header_t;
 
-void *create_block(size_t);
+header_t *create_block(size_t);
 
-void *malloc(size_t);
-void *realloc(void *, size_t);
-void free(void *);
+void *my_malloc(size_t);
+void *my_realloc(void *, size_t);
+void my_free(void *);
 
 uint8_t heap[MAX_HEAP_SIZE];
 size_t heap_top = 0;
@@ -24,21 +24,50 @@ header_t *past_block = NULL;
 
 int main()
 {
+    void *ptrs[4];
+    void *ptr = NULL;
 
-    void *ptr = malloc(10);
+    for (uint8_t i = 1; i <= 4; i++)
+    {
+        printf("---------\n");
+        ptr = my_malloc(5 * i);
+        ptrs[i - 1] = ptr;
+        printf("%d cycle\n", i);
+        printf("---------\n\n");
+    }
+    for (uint8_t i = 1; i <= 4; i++)
+    {
+        header_t *ptr_header = (header_t *)((uint8_t *)ptrs[i - 1] - sizeof(header_t));
+        printf("Ptr%d info:\n", i);
+        // printf("Ptr address: \t\t%p\n", ptrs[i]);
+        printf("Header address: \t%p\n", ptr_header);
+        printf("Ptr%d data size: \t%d\n", i, ptr_header->data_size);
+        printf("Ptr%d prev block: \t%p\n", i, ptr_header->prev);
+        printf("Ptr%d next block: \t%p\n\n", i, ptr_header->next);
+    }
 
-    // header_t *ptr_header = (header_t *)((uint8_t *)ptr - sizeof(header_t));
-    printf("\nPtr address: %p\n\n", ptr);
+    my_free(ptrs[2]); // 15 byte
+    void *new_ptr = my_realloc(ptrs[1], 26);
+    if (new_ptr)
+    {
+        printf("\n\nYes\n\n");
+        ptrs[1] = new_ptr;
+    }
 
-    // printf("Ptr1 info:\n");
-    // printf("Header address: %p\n", ptr_header);
-    // printf("Ptr1 data size: %d\n", ptr_header->data_size);
-    // printf("Ptr1 next block: %p\n", ptr_header->next);
-    // printf("Ptr1 past block: %p\n", ptr_header->prev);
+    for (uint8_t i = 1; i <= 4; i++)
+    {
+        header_t *ptr_header = (header_t *)((uint8_t *)ptrs[i - 1] - sizeof(header_t));
+        printf("Ptr%d info:\n", i);
+        // printf("Ptr address: \t\t%p\n", ptrs[i]);
+        printf("Header address: \t%p\n", ptr_header);
+        printf("Ptr%d data size: \t%d\n", i, ptr_header->data_size);
+        printf("Ptr%d prev block: \t%p\n", i, ptr_header->prev);
+        printf("Ptr%d next block: \t%p\n\n", i, ptr_header->next);
+    }
     return 0;
 }
 
-void *create_block(size_t data_size)
+header_t *create_block(size_t data_size)
 {
     header_t *current_block = (header_t *)&heap[heap_top];
     current_block->data_size = data_size;
@@ -50,22 +79,13 @@ void *create_block(size_t data_size)
     return current_block;
 }
 
-void *malloc(size_t data_size)
+void *my_malloc(size_t data_size)
 {
     void *data_section = NULL;
-
-    if (!first_block)
+    if (first_block == NULL)
     {
-        first_block = create_block(data_size);
-
-        // printf("First block size: %d\n", first_block->data_size);
-        // printf("First block next block: %p\n", first_block->next);
-        // printf("First block past block: %p\n", first_block->prev);
-
-        // printf("First block address: %p\n", first_block);
+        first_block = (header_t *)create_block(data_size);
         data_section = ((uint8_t *)first_block + sizeof(header_t));
-        // printf("First data section address: %p\n", data_section);
-        // printf("First block size: %d\n", ((uint8_t *)data_section + data_size) - (uint8_t *)first_block);
     }
     else
     {
@@ -73,8 +93,9 @@ void *malloc(size_t data_size)
         heap_top = sizeof(header_t) + temp_block->data_size;
         while (temp_block->next)
         {
-            printf("Heap top: %d\n", heap_top);
-            if (((uint8_t *)temp_block->next) - ((uint8_t *)temp_block + sizeof(header_t) + temp_block->data_size) >= data_size + sizeof(header_t))
+            size_t diff = (uintptr_t)temp_block->next - ((uintptr_t)temp_block + sizeof(header_t) + temp_block->data_size);
+            printf("Diff: %d", diff);
+            if (diff >= data_size + sizeof(header_t))
             {
                 printf("It fit!\n");
                 data_section = ((uint8_t *)create_block(data_size) + sizeof(header_t));
@@ -83,16 +104,54 @@ void *malloc(size_t data_size)
             else
             {
                 printf("Na-h...\n");
-                heap_top += (uint8_t *)temp_block->next - (uint8_t *)temp_block - sizeof(header_t) - temp_block->data_size;
+                printf("\ndif : %d\n", sizeof(header_t) + (temp_block->next)->data_size);
+                heap_top += sizeof(header_t) + (temp_block->next)->data_size + diff; // offset heap_top point on top of the next block
                 temp_block = temp_block->next;
             }
         }
-        printf("Heap top: %d\n", heap_top);
 
         if (heap_top + sizeof(header_t) + data_size > MAX_HEAP_SIZE)
-            return NULL;
+            return data_section;
 
         data_section = ((uint8_t *)create_block(data_size) + sizeof(header_t));
     }
     return data_section;
+}
+
+void *my_realloc(void *ptr, size_t block_size)
+{
+    if (ptr == NULL)
+        return my_malloc(block_size);
+    void *new_ptr = NULL;
+
+    header_t *current_block = (header_t *)((uint8_t *)ptr - sizeof(header_t));
+
+    if ((uintptr_t)current_block->next - (uintptr_t)ptr < block_size)
+    {
+        new_ptr = my_malloc(block_size);
+        if (new_ptr)
+        {
+            for (size_t i = 0; i < current_block->data_size && i < block_size; i++)
+            {
+                *((uint8_t *)new_ptr + i) = *((uint8_t *)ptr + i);
+            }
+            my_free(ptr);
+            return new_ptr;
+        }
+        return NULL;
+    }
+    current_block->data_size = block_size;
+    return ptr;
+}
+
+void my_free(void *ptr)
+{
+    header_t *current_block = (header_t *)((uint8_t *)ptr - sizeof(header_t));
+    if (current_block == first_block)
+        first_block = current_block->next;
+    header_t *prev_block = current_block->prev;
+    header_t *next_block = current_block->next;
+    prev_block->next = next_block;
+    next_block->prev = prev_block;
+    memset(current_block, 0, current_block->data_size + sizeof(header_t));
 }
