270afb639692670aad7feb2f2cc56b2fd583eb4f
Denovsky <ilya20002282000@mail.ru>
2025-07-25 05:17:05 +0300

    iam give up, sry

--- allocator.c
+++ allocator.c
@@ -27,14 +27,14 @@ int main()
 
     void *ptr = malloc(10);
 
-    header_t *ptr_header = (header_t *)((uint8_t *)ptr - sizeof(header_t));
+    // header_t *ptr_header = (header_t *)((uint8_t *)ptr - sizeof(header_t));
     printf("\nPtr address: %p\n\n", ptr);
 
-    printf("Unit1 info:\n");
-    printf("Header address: %p\n", ptr_header);
-    printf("Unit1 data size: %d\n", ptr_header->data_size);
-    printf("Unit1 next block: %p\n", ptr_header->next);
-    printf("Unit1 past block: %p\n", ptr_header->prev);
+    // printf("Ptr1 info:\n");
+    // printf("Header address: %p\n", ptr_header);
+    // printf("Ptr1 data size: %d\n", ptr_header->data_size);
+    // printf("Ptr1 next block: %p\n", ptr_header->next);
+    // printf("Ptr1 past block: %p\n", ptr_header->prev);
     return 0;
 }
 
@@ -63,31 +63,31 @@ void *malloc(size_t data_size)
         // printf("First block past block: %p\n", first_block->prev);
 
         // printf("First block address: %p\n", first_block);
-        data_section = ((uint8_t *)create_block(data_size) + sizeof(header_t));
+        data_section = ((uint8_t *)first_block + sizeof(header_t));
         // printf("First data section address: %p\n", data_section);
         // printf("First block size: %d\n", ((uint8_t *)data_section + data_size) - (uint8_t *)first_block);
     }
     else
     {
         header_t *temp_block = first_block;
-        heap_top = 0;
+        heap_top = sizeof(header_t) + temp_block->data_size;
         while (temp_block->next)
         {
-            heap_top += sizeof(header_t) + temp_block->data_size;
-            // printf("Heap top: %d\n", heap_top);
+            printf("Heap top: %d\n", heap_top);
             if (((uint8_t *)temp_block->next) - ((uint8_t *)temp_block + sizeof(header_t) + temp_block->data_size) >= data_size + sizeof(header_t))
             {
-                // printf("It fit!\n");
+                printf("It fit!\n");
                 data_section = ((uint8_t *)create_block(data_size) + sizeof(header_t));
-                break;
+                return data_section;
             }
             else
             {
-                // printf("Na-h...\n");
+                printf("Na-h...\n");
                 heap_top += (uint8_t *)temp_block->next - (uint8_t *)temp_block - sizeof(header_t) - temp_block->data_size;
                 temp_block = temp_block->next;
             }
         }
+        printf("Heap top: %d\n", heap_top);
 
         if (heap_top + sizeof(header_t) + data_size > MAX_HEAP_SIZE)
             return NULL;
