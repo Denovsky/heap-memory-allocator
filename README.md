c6160659b4e4d382735940803599e5ddaa390e8c
Denovsky <ilya20002282000@mail.ru>
2025-08-08 19:23:24 +0300

    +inner commands handler; +input command validation.

--- README.md
+++ README.md
@@ -6,11 +6,23 @@ Status: 90% completed. Main logic is done (malloc, realloc, free, memset).
 
 Plans:
 
-0. ~~Malloc/realloc realization;~~
-1. ~~Memory cleaner (free func);~~
-2. ~~Separate memory allocation logic from main file;~~
-3. ~~Add header block for meta-info;~~
-4. ~~Fix malloc Segmentaion fault after else condition;~~
+1. ~~Malloc/realloc realization;~~
+2. ~~Memory cleaner (free func);~~
+3. ~~Separate memory allocation logic from main file;~~
+4. ~~Add header block for meta-info;~~
+5. ~~Fix malloc Segmentaion fault after else condition;~~
 6. ~~Memset realization;~~  
-5.   Memory defragmentation realization;
-7.   ...
+7.   Memory defragmentation realization;
+8.   ...
+
+
+Command line and command processing.
+
+Status: in work. Main logic of command validation and processing is done. Must be completed main commands logic.
+
+1. ~~Receive/Transmit symbols logic;~~
+2. ~~Command validation;~~
+3. ~~Commands handler;~~
+4.   Commands processing;
+5.   Separate logic and make encapsulation;
+6.   ...
--- main.c
+++ main.c
@@ -1,53 +1,82 @@
 #include <stdio.h>
 #include "allocator.h"
 
-char *receive_input();
+char *RXData();      // receive symbol by symbol from UART/USART
+void TXData(char *); // transmit symbol by symbol by UART/USART
+
+/*----------*/
+/* main command validation funcs: */
 char *getMainCommand(char *);
 char *getSubCommand(char *);
 uint8_t compareStr(const char *, char *);
-void outputStr(char *, char);
-
-void led_handler();
-void help_handler();
+/*----------*/
+/* main command processing funcs: */
+void led_handler(char **);
+void help_handler(char **);
+void echo_handler(char **);
+/*----------*/
 
 typedef struct map
 {
     const char *key;
-    void (*handler)(/*char **args, int argc*/); // add (char **args, int argc) logic later
+    void (*handler)(char **args /*, int argc*/); // add (char **args, int argc) logic later
 } map_t;
 
 map_t cmds[] = {
     {"led", led_handler},
-    {"help", help_handler}};
+    {"help", help_handler},
+    {"echo", echo_handler}};
 
 int main()
 {
-    char *input = receive_input();
-    // char input[] = "led on";
-    char *main_command = getMainCommand(input);
-    char *sub_command = getSubCommand(input);
-
-    for (uint8_t i = 0; i < sizeof(cmds) / (sizeof(const char *) + sizeof(void (*)(char **))); i++)
+    uint8_t cmds_size = sizeof(cmds) / (sizeof(const char *) + sizeof(void (*)(char **)));
+    while (1)
     {
+        char *input = RXData();
+        // char input[] = "led on";
+        char *main_command = getMainCommand(input);
+        char *sub_command = getSubCommand(input);
 
-        if (compareStr(cmds[i].key, main_command))
+        uint8_t isWork = 0;
+        for (uint8_t i = 0; i < cmds_size; i++)
         {
-            cmds[i].handler();
+            if (compareStr(cmds[i].key, main_command))
+            {
+                char *args[] = {"", &cmds_size, sub_command};
+                isWork = 1;
+                cmds[i].handler(args);
+            }
+        }
+        if (!isWork)
+        {
+            TXData("No such command");
         }
     }
-
     return 0;
 } // Test phrase: Somebody once told me that world is gonna rule me
 
-void led_handler()
+void led_handler(char **args)
 {
-    const char *led_params[] = {"on", "off"};
-    printf("led\n");
+    const char *params[] = {"on", "off"};
+    TXData("There is no working leds.");
 }
 
-void help_handler()
+void help_handler(char **args)
 {
-    printf("help\n");
+    // const char *params[] = NULL; // no params because of cmds list is params
+    TXData("There is no escape!");
+}
+
+void echo_handler(char **args)
+{
+    if (args[2] != NULL)
+    {
+        TXData(args[2]);
+    }
+    else
+    {
+        TXData("Nothin to print");
+    }
 }
 
 char *getSubCommand(char *input)
@@ -67,7 +96,7 @@ char *getSubCommand(char *input)
         i++;
         symbol = *(input + i);
         *(buffer + j) = symbol;
-        while (symbol != ' ' && symbol != '\0')
+        while (/* symbol != ' ' && */ symbol != '\0')
         {
             symbol = *(input + i);
             if (buffer_size >= j)
@@ -75,14 +104,15 @@ char *getSubCommand(char *input)
                 buffer_size += 10;
                 buffer = realloc_(buffer, buffer_size);
             }
-            if (symbol == ' ')
-            {
-                *(buffer + i) = '\0';
-            }
-            else
-            {
-                *(buffer + j) = symbol;
-            }
+            // if (symbol == ' ')
+            // {
+            //     *(buffer + j) = '\0';
+            // }
+            // else
+            // {
+            //     *(buffer + j) = symbol;
+            // }
+            *(buffer + j) = symbol;
             i++;
             j++;
         }
@@ -118,7 +148,7 @@ char *getMainCommand(char *input)
     return buffer;
 }
 
-char *receive_input()
+char *RXData()
 {
     uint16_t RX_data_buffer_size = 10;
     char *RX_data_buffer = malloc_(RX_data_buffer_size);
@@ -135,10 +165,24 @@ char *receive_input()
         *(RX_data_buffer + index) = DR;
         index++;
     }
-    *(RX_data_buffer + index - 1) = '\0';
+    if (*(RX_data_buffer + index - 1) == '\n')
+        *(RX_data_buffer + index - 1) = '\0';
     // header_t *current_block = (header_t *)((uint8_t *)RX_data_buffer - sizeof(header_t)); // check that allocator is correct
     // printf("%d", current_block->data_size);
     return RX_data_buffer;
+}
+
+void TXData(char *ptr)
+{
+    uint16_t index = 0;
+    char symbol = *ptr;
+    while (symbol != '\0')
+    {
+        printf("%c", symbol, symbol); // Data Register
+        index++;
+        symbol = *(ptr + index);
+    }
+    printf("\n"); // Data Register
 }
 
 uint8_t compareStr(const char *command_ptr, char *input_ptr)
@@ -157,16 +201,4 @@ uint8_t compareStr(const char *command_ptr, char *input_ptr)
     if (command_symbol != input_symbol)
         return 0;
     return 1;
-}
-
-void outputStr(char *ptr, char split_symbol)
-{
-    uint16_t index = 0;
-    while (*(ptr + index) != split_symbol)
-    {
-        printf("%c - %d\n", *(ptr + index), *(ptr + index));
-        index++;
-    }
-    printf("%c - %d\n", *(ptr + index), *(ptr + index));
-    printf("\n");
 }
